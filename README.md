492d4cde26b7fa07dcfe877b6890ca92d801e720
ramnanu179 <ramnanu179@gmail.com>
2024-09-19 10:54:36 +0530

    Added Documentation for the api

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-teaasass
+# Problem statement
+This service provide a reward program to the customers, awarding points to the customers based on each transaction for a given time.
+
+A customer receives 2 points for every dollar spent over $100 in each transaction, plus 1 point for every dollar spent between $50 and $100 in each transaction.
+(e.g. a $120 purchase = 2*$20 + 1*$50 = 90 Points).
+
+## Technology used:
++ Java 17
++ Springboot 3.3.3
++ Maven
++ MariaDB 11.5.2-MariaDB
+
+
+## Entity Diagram:
+![Flow Diagram 1](images/entity-diagram.png)
+
+#### Data flow diagram:
+![Flow Diagram 2](images/data-flow-diagram.png)
+
+## API Documentation:
+
+ Get Rewards:
+ 
+    + URI: "api/1.1/reward/customer/[custId]?months=[months]"
+    + Month default value 3, if not specified(optional).
+    + Calculate and return rewards for a customer in below format.
+    + Response Body:
+   ```json
+       {
+		    "name": "Rakesh",
+		    "year": 2024,
+		    "totalPoints": 360,
+		    "monthWisePoints": [{
+		            "month": "MAY",
+		            "points": 110
+		        },
+		        {
+		            "month": "JULY",
+		            "points": 180
+		        },
+		        {
+		            "month": "APRIL",
+		            "points": 70
+		        }
+		     ]
+		   }
+		   
+    
--- src/test/java/com/infosys/retailApp/services/RewardServiceTest.java
+++ src/test/java/com/infosys/retailApp/services/RewardServiceTest.java
@@ -1,25 +1,95 @@
 package com.infosys.retailApp.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import com.infosys.retailApp.dto.response.CustomerRewards;
+import com.infosys.retailApp.entity.Customer;
+import com.infosys.retailApp.entity.Transaction;
+import com.infosys.retailApp.exception.CustomerNotFoundException;
 import com.infosys.retailApp.repository.CustomerRepository;
 import com.infosys.retailApp.repository.TransactionRepository;
 
 public class RewardServiceTest {
+	
+	@Mock
+	private TransactionRepository transactionRepository;
+
+	@Mock
+	private CustomerRepository customerRepository;
+	
 	@InjectMocks
-    private RewardService rewardService;
+	private RewardServiceImpl rewardServiceImpl;
 
-    @Mock
-    private TransactionRepository transactionRepository;
+	@BeforeEach
+	void setUp() {
+		MockitoAnnotations.openMocks(this);
+	}
 
-    @Mock
-    private CustomerRepository customerRepository;
+	@Test
+	void testGetCustomerRewards_InvalidCustomerId() {
+		BigInteger custId = new BigInteger("2");
+		when(customerRepository.findById(custId)).thenReturn(Optional.ofNullable(null));
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
+		CustomerNotFoundException exception = assertThrows(CustomerNotFoundException.class,
+				() -> rewardServiceImpl.getRewards(custId, 12));
+		assertNotNull(exception);
+	}
+
+	@Test
+	void testGetRewards() {
+		BigInteger custId = new BigInteger("1");
+		Customer customer = new Customer();
+
+		customer.setName("Rajesh");
+		customer.setId(custId);
+		customer.setEmailId("rajesh1@gmail.com");
+		customer.setPhNumber("1232424");
+
+		Transaction transaction = new Transaction();
+		transaction.setId(UUID.randomUUID());
+		transaction.setCustomer(customer);
+		transaction.setAmount(new BigDecimal("180"));
+		transaction.setDate(LocalDateTime.now().minusMonths(1));
+
+		Transaction transaction2 = new Transaction();
+		transaction2.setId(UUID.randomUUID());
+		transaction2.setCustomer(customer);
+		transaction2.setAmount(new BigDecimal("50"));
+		transaction2.setDate(LocalDateTime.now().minusMonths(2));
+
+		Transaction transaction3 = new Transaction();
+		transaction3.setId(UUID.randomUUID());
+		transaction3.setCustomer(customer);
+		transaction3.setAmount(new BigDecimal("60"));
+		transaction3.setDate(LocalDateTime.now().minusMonths(5));
+
+		when(customerRepository.findById(custId)).thenReturn(Optional.of(customer));
+		when(transactionRepository.findByCustomerIdAndDateAfter(eq(custId), any(LocalDateTime.class)))
+				.thenReturn(Arrays.asList(transaction, transaction2, transaction3));
+
+		CustomerRewards customerRewards = rewardServiceImpl.getRewards(custId, 6);
+
+		assertNotNull(customerRewards);
+		assertEquals(220, customerRewards.getTotalPoints());
+		assertEquals(3, customerRewards.getMonthWisePoints().size());
+	}
+
 }
